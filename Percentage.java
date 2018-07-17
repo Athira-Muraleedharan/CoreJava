e2e3080160b630f2b1102e233778e94162ac95b1
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-17 17:02:44 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,14 @@
+
+public class Percentage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m1=20,m2=34,m3=12,m4=78;
+		double m5=45.9;
+		double perc;
+		perc=(m1+m2+m3+m4+m5)/5;
+		System.out.println("Percentage of a Student in 5 subjects: "+perc);
+        
+	}
+
+}
