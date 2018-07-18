af63f8ff6ec5ccd49aa9a79a49265ade2d1fdb76
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-18 23:43:49 +0530

    Add files via upload

---
+++ BiggestNumber.java
@@ -0,0 +1,15 @@
+
+public class BiggestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {12,50,2,5,55,25};
+		int sum=0;
+		for (int i=0;i<arr.length;i++) {
+			if(arr[i]%2==0)
+				sum+=arr[i];
+		}
+		System.out.println(sum);
+	}
+
+}
