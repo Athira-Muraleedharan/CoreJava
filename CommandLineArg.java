c2f9ff6ba8add8b5a532b53b6539c431d98851b4
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-19 17:02:30 +0530

    Add files via upload

---
+++ CommandLineArg.java
@@ -0,0 +1,11 @@
+
+public class CommandLineArg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You have passed..."+args.length+"arguments");
+		for(int i=0;i<args.length;i++)
+			System.out.println("Data is "+ args[i]);
+	}
+
+}
