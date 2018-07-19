a1641a0907de00e7b6f78a213c2d08682ff03453
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-19 13:27:28 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,17 @@
+
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student();
+		s1.m1=78;
+		s1.m2=45;
+		s1.m3=70;
+		s1.total=s1.m1+s1.m2+s1.m3;
+		s1.calGrade();
+		s1.getResult();
+		s1.show();
+
+	}
+
+}
