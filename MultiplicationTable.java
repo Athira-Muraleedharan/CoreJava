f7a08bbe97b8f2b0173abd5df4c1f45fc9f263ab
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-19 15:09:50 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,9 +3,13 @@ public class MultiplicationTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 
-		 
+		//Multiplication Table of 5
+		for(int i=1;i<=10;i++) {
+			System.out.println("5 * "+i+" = "+(5*i));
 		}
+		  
+		 
 	}
+}
 
 
