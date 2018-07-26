8ae5be83d614970c586208da66e817522ff6e9af
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-26 15:49:30 +0530

    Add files via upload

---
+++ ExerciseDemo.java
@@ -0,0 +1,17 @@
+//Program to check whether the password and username are valid using command line arguments
+public class ExerciseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args[0].equals("Athira"))
+			{
+				if(args[1].equals("Deloitte"))
+					System.out.println("You are a Valid User");
+			}
+		else
+				System.out.println("You are not a valid user");
+		
+
+	}
+
+}
