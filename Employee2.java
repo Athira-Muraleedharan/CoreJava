8ca57be2c4053b6b6c456197eaddc66004f2bbd9
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-26 15:39:33 +0530

    Add files via upload

---
+++ Employee2.java
@@ -0,0 +1,12 @@
+
+public class Employee2 {
+	String name;
+	Employee2(String nm){
+		name=nm;
+	}
+	public static void main(String args[]) {
+		Employee2 e=new Employee2("nm");
+		System.out.println(e.name);
+	}
+}
+
