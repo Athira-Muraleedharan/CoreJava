d5311ab74856a0fe59b71ace99b74b47aa7d041c
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-26 15:59:32 +0530

    Add files via upload

---
+++ InterfaceDemo.java
@@ -0,0 +1,17 @@
+
+public class InterfaceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Sony s=new Sony();
+		Samsung s1=new Samsung();
+		
+		s.switchOn();
+		s.switchOff();
+		s.changeChannel();
+		s1.switchOff();
+		s1.switchOn();
+	}
+
+}
