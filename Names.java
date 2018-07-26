f10784d6d7f7ad04446a8d465a933aac70d1b843
Athira-Muraleedharan <41305349+Athira-Muraleedharan@users.noreply.github.com>
2018-07-26 17:12:29 +0530

    Add files via upload

---
+++ Names.java
@@ -0,0 +1,13 @@
+
+public class Names {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int superm1_sale=15000,superm2_sale=115*75;
+		int match=superm1_sale-superm2_sale;
+		int items=match/75;
+		System.out.println(items);
+		}
+	}
+
+
